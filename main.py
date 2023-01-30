ac2d85053faca2f6bd971c8e8a55192db8cae1f8
Jeremy-Pritt <jfpritt@gmail.com>
2023-01-30 08:31:35 -0700

    added year and month user input

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ with pre_recorded_tab:
     pre_recorded_form = st.form("Upload a Speech Sample")
     with pre_recorded_form:
         speech_sample = st.file_uploader("Please upload an audio file of your child\'s speech:")
-        age = st.text_input("Please enter your child's age:")
+        st.write("Please enter your child's age:")
+        age_year = st.selectbox(label="Years:", options=("4", "5", "6", "7", "8", "9", "10", "11"))
+        age_month = st.selectbox(label="Months:", options=("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"))
         submission = st.form_submit_button("Submit and Run Screening")
+        if submission == True:
+            # logic goes here for processing speech sample
 
 with new_recording_tab:
     new_recording_form = st.form('Make and Upload a Speech Sample')
@@ -23,8 +27,12 @@ with new_recording_tab:
         wav_audio_data = st_audiorec()
         if wav_audio_data is not None:
             st.audio(wav_audio_data, format='audio/wav')
-        age_new = st.text_input("Please enter your child\'s age:")
+        st.write("Please enter your child's age:")
+        age_year_new = st.selectbox(label="Years:", options=("4", "5", "6", "7", "8", "9", "10", "11"))
+        age_month_new = st.selectbox(label="Months:", options=("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"))
         submission_new = st.form_submit_button("Submit and Run Screening")
+        if submission_new == True:
+            # logic goes here for processing speech sample
 
 
 
