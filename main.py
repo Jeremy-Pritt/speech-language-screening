ce0d5ce4303edc39c1bfa9aeccbd99cba19d7939
Jeremy-Pritt <jfpritt@gmail.com>
2023-01-29 21:35:15 -0700

    add streamlit app with file uploader

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+st.title("Speech-Language Screening")
+speech_sample = st.file_uploader("Please upload child's speech sample:")
+
+
