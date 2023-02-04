e728ca3b757bb5f91b7637adec2be0155ae4fa48
Jeremy-Pritt <jfpritt@gmail.com>
2023-02-04 09:51:14 -0700

    fixed for real now

--- upload_and_save_wavefiles.py
+++ upload_and_save_wavefiles.py
@@ -12,13 +12,3 @@ def upload_and_save_wavefiles(uploaded_file):
             return audio
     else:
         return "empty file"
-
-# def upload_and_save_wavefiles(uploaded_file):
-#     """ limited 200MB, you could increase by `streamlit run foo.py --server.maxUploadSize=1024` """
-#     if uploaded_file is not None:
-#         try:
-#             sound = pydub.AudioSegment.from_mp3(uploaded_file)
-#             sound.export("file.wav", format = "wav")
-#             return None
-#         except:
-#             sound =
