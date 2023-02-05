8c2a97411100dae75c9eb362a08cff804e914e5c
Jeremy-Pritt <jfpritt@gmail.com>
2023-02-04 21:42:31 -0700

    added frame_rate param

--- convert_bytes_to_wav.py
+++ convert_bytes_to_wav.py
@@ -4,5 +4,5 @@ from pydub import AudioSegment
 def convert_bytes_to_wav(raw_sound):
     s = io.BytesIO(raw_sound)
     filename = "mic_input.wav"
-    audio = AudioSegment.from_raw(s, sample_width=16).export(filename, format='wav')
+    audio = AudioSegment.from_raw(s, sample_width=16, frame_rate=16000).export(filename, format='wav')
     return audio
