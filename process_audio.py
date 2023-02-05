1b4d1f6324c54dd0cdf35589ef5bb23de2627993
Jeremy-Pritt <jfpritt@gmail.com>
2023-02-04 19:50:37 -0700

    added librosa compatability with mp3 files

--- process_audio.py
+++ process_audio.py
@@ -1,5 +1,6 @@
 import numpy as np
 import librosa
+from pydub import AudioSegment
 
 def process_audio(uploaded_file):
     "200 mb limit for files"
@@ -9,9 +10,19 @@ def process_audio(uploaded_file):
     :return: two varialbes are returned; the first is an array of the samples and the second is the sampling rate
     """
     if uploaded_file is not None:
-        x, sr = librosa.load(uploaded_file, sr=16000)
-        samples_arry = np.array(x)
-        return samples_arry, sr
+        if uploaded_file.name.endswith('wav'):
+            x, sr = librosa.load(uploaded_file, sr=16000)
+            samples_arry = np.array(x)
+            return samples_arry, sr
+        elif uploaded_file.name.endswith('mp3'):
+            output_file = 'result.wav'
+            sound = AudioSegment.from_mp3(uploaded_file)
+            sound.export(output_file, format="wav")
+            x, sr = librosa.load(uploaded_file, sr=16000)
+            samples_arry = np.array(x)
+            return samples_arry, sr
+
+
 
 # def process_audio(uploaded_file):
 #     "200 mb limit for files"
