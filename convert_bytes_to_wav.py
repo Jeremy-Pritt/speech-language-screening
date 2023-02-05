775a0443769ff1ec9bdd108d35c8c229067f5e35
Jeremy-Pritt <jfpritt@gmail.com>
2023-02-04 21:55:21 -0700

    adjusted parameters for reading in the microphone audio

--- convert_bytes_to_wav.py
+++ convert_bytes_to_wav.py
@@ -4,5 +4,5 @@ from pydub import AudioSegment
 def convert_bytes_to_wav(raw_sound):
     s = io.BytesIO(raw_sound)
     filename = "mic_input.wav"
-    audio = AudioSegment.from_raw(s, sample_width=2, frame_rate=16000, channels=1).export(filename, format='wav')
+    audio = AudioSegment.from_raw(s, sample_width=4, frame_rate=44100, channels=2).export(filename, format='wav')
     return audio
