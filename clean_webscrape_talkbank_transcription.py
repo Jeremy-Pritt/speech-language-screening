f2a52d6faf2470238a548d3d655189001b8743ed
Jeremy-Pritt <jfpritt@gmail.com>
2023-02-12 08:47:32 -0700

    added script to split csv files into parts

--- clean_webscrape_talkbank_transcription.py
+++ clean_webscrape_talkbank_transcription.py
@@ -4,46 +4,75 @@ from selenium import webdriver
 import time
 
 def clean_webscrape_talkbank_transcription(csv_file_path, save_file_name):
+    """
+    webscrapes TalkBank transcription data
+    :param csv_file_path: csv file from TalkBankDB with transcription pages metadata
+    :param save_file_name: file path to the location the outputted csv file will be save to
+    :return: csv file with webscraped transcription data for each transcription page in input csv file
+    """
     # read in the csv file as a df
-    df = pd.read_csv(csv_file_path, delim_whitespace=True, header=None)
+    df = pd.read_csv(csv_file_path)
 
     # remove unimportant columns from the df
-    useless_columns = [1, 3, 4, 6]
+    useless_columns = ["Unnamed: 0", "index", "1", "4", "5", "3"]
     for col in useless_columns:
         df.pop(col)
 
     # rename the columns
-    df.columns = ['path_to_transcript', 'language', 'transcript_PID', 'activity_type', 'group_type']
+    df.columns = ['path_to_transcript', 'language', 'study_type', 'activity_type', 'group_type']
 
     # include english only
     df = df[df['language'] == 'eng']
 
-    # initialize a new column for the transcription
+    # initialize new columns
+    df['age'] = ""
+    df['sex'] = ""
     df['transcription'] = ""
 
-    # add transcription to each observation
+    # loop through each transcription webpage in the dataframe
     for i, row in df.iterrows():
-        path = row.path_to_transcript
-        # get web page
-        url = "https://sla.talkbank.org/TBB/" + path + ".cha"
+        try:
 
-        ############### Begin Selenium
-        driver = webdriver.Firefox()
-        driver.get(url)
-        time.sleep(5)
-        page = driver.page_source
-        driver.quit()
-        ############### End of Selenium
+            path = row.path_to_transcript
+            # get web page
+            url = "https://sla.talkbank.org/TBB/" + path + ".cha"
 
-        soup = BeautifulSoup(page, 'html.parser')
+            # open and read in the transcription webpage
+            driver = webdriver.Firefox()
+            driver.get(url)
+            time.sleep(6)
+            page = driver.page_source
+            driver.quit()
 
-        utt_list = []
-        for utterance in soup.findAll(class_="utterance"):
-            if utterance.span.strong.get_text(strip=True) == 'CHI:':
-                utt_list.append(utterance.span.get_text(strip=False))
+            # parse the web page HTML
+            soup = BeautifulSoup(page, 'html.parser')
 
-        df['transcription'].loc[i] = utt_list
+            # extract transcription from soup
+            utt_list = []
+            for utterance in soup.findAll(class_="utterance"):
+                if utterance.span.strong.get_text(strip=True) == 'CHI:':
+                    utt_list.append(utterance.span.get_text(strip=False))
 
-    print(df['transcription'])
+            # extract age and sex from soup
+            age = ""
+            sex = ""
+            metaDataTable = soup.findAll(class_="metaDataTable")[1]
+            for tr_tag in metaDataTable.find_all('tr')[1:]:
+                if tr_tag.td.text == 'CHI':
+                    age = tr_tag.select("td:nth-of-type(" + str(5) + ")")[0].text
+                    sex = tr_tag.select("td:nth-of-type(" + str(6) + ")")[0].text
+
+            # update dataframes with extracted data
+            df['age'].loc[i] = age
+            df['sex'].loc[i] = sex
+            df['transcription'].loc[i] = utt_list
+
+        except:
+            print("skipped at this index:", i)
+            continue
+
+    # save to the specified file path, save_file_name
     df.to_csv(save_file_name)
 
+# call
+clean_webscrape_talkbank_transcription(r"TalkBank Transcripts MetaData/language_disorders_split_2.csv", r"Clean TalkBank Data/language_disorders_scraped_split_2.csv")
---
+++ split_csv_data.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+def rec_add(lst):
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        return lst[0] + rec_add(lst[1:])
+
+def split_csv_data(csv_file, num_splits, new_file_path):
+    df_tot = pd.read_csv(csv_file, delim_whitespace=True, header=None)
+    column_names = df_tot.columns
+    num_rows = df_tot.shape[0]
+    file_splits_len = []
+    if num_rows % num_splits == 0:
+        for i in range(num_splits):
+            file_splits_len.append(int(num_rows / num_splits))
+    else:
+        for i in range(num_splits - 1):
+            file_splits_len.append(int((num_rows // num_splits)))
+        file_splits_len.append(num_rows - sum(file_splits_len))
+
+    splits = []
+    splits.append(file_splits_len[0])
+
+    for i in range(num_splits):
+        if i == 0:
+            continue
+        else:
+            x = rec_add(file_splits_len[0:i+1])
+            splits.append(x)
+
+    pandas_dfs = []
+    for i in range(num_splits):
+        if i == 0:
+            split_df = df_tot.iloc[0:splits[i]]
+            split_df.reset_index(inplace=True)
+            pandas_dfs.append(split_df)
+        else:
+            split_df = df_tot.iloc[splits[i-1]:splits[i]]
+            split_df.reset_index(inplace=True)
+            pandas_dfs.append(split_df)
+
+    for i in range(num_splits):
+        pandas_dfs[i].to_csv(new_file_path + "_split_" + str(i+1) + ".csv")
+
+
+csv_file = r"TalkBank Transcripts MetaData/language_disorders.csv"
+new_file_path = r"TalkBank Transcripts MetaData/language_disorders"
+split_csv_data(csv_file, 3, new_file_path)
+
